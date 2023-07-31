10287fd6717b43c0f5744c80d8c2bff5bbc42d3d
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-07-30 23:36:32 -0700

    Completed Destruction

--- javascript/fundamentals/predictDestructuring.js
+++ javascript/fundamentals/predictDestructuring.js
@@ -2,8 +2,8 @@ const cars = ["Tesla", "Mercedes", "Honda"];
 const [randomCar] = cars;
 const [, otherRandomCar] = cars;
 //Predict the output
-console.log(randomCar);
-console.log(otherRandomCar);
+console.log(randomCar); // Tesla
+console.log(otherRandomCar); // Mercedes
 
 const employee = {
   name: "Elon",
@@ -12,8 +12,8 @@ const employee = {
 };
 const { name: otherName } = employee;
 //Predict the output
-console.log(name);
-console.log(otherName);
+console.log(name); // not defined
+console.log(otherName); // Elon
 
 const person = {
   name: "Phil Smith",
@@ -23,26 +23,26 @@ const person = {
 const password = "12345";
 const { password: hashedPassword } = person;
 //Predict the output
-console.log(password);
-console.log(hashedPassword);
+console.log(password); // 12345
+console.log(hashedPassword); // undefined
 
 const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
-const [, first] = numbers;
-const [, , , second] = numbers;
-const [, , , , , , , , third] = numbers;
+const [, first] = numbers; // 2
+const [, , , second] = numbers; // 5
+const [, , , , , , , , third] = numbers; // 2
 //Predict the output
-console.log(first == second);
-console.log(first == third);
+console.log(first == second); // false
+console.log(first == third); // true
 
 const lastTest = {
   key: "value",
   secondKey: [1, 5, 1, 8, 3, 3],
 };
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [, willThisWork] = secondKey;
+const { key } = lastTest; // value
+const { secondKey } = lastTest; // [1, 5, 1, 8, 3, 3]
+const [, willThisWork] = secondKey; // 5
 //Predict the output
-console.log(key);
-console.log(secondKey);
-console.log(secondKey[0]);
-console.log(willThisWork);
+console.log(key); // value
+console.log(secondKey); // [1, 5, 1, 8, 3, 3]
+console.log(secondKey[0]); // 1
+console.log(willThisWork); // 5
