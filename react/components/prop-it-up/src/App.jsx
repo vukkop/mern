ffb53d778fcd0194a34f5a0b6b15f0da4fb54fb7
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-08-02 10:28:13 -0700

    Prop It Up

--- react/components/prop-it-up/src/App.jsx
+++ react/components/prop-it-up/src/App.jsx
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <h1>Person Cards</h1>
+      <PersonCard firstName={"Doe"} lastName={"Jane"} age={30} hairColor={"Black"}/>
+      <PersonCard firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
     </div>
   );
 }
