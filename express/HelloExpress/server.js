d881a943e35db7836cbc255dffe6e5b02d3529a0
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-08-08 15:04:11 -0700

    HelloExpress

---
+++ express/HelloExpress/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+// req is short for request
+// res is short for response
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+const server = app.listen(port, () =>
+  console.log(`Server is locked and loaded on port ${port}!`)
+);
