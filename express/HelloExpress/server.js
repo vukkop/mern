9dd03833b95d663bab2e193d169c50b374915053
Vukasin Koprivica <koprivica.vukasin@gmail.com>
2023-08-09 14:09:39 -0700

    MongoDB

--- express/HelloExpress/server.js
+++ express/HelloExpress/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//config
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // req is short for request
 // res is short for response
 app.get("/api", (req, res) => {
